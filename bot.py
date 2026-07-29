5404eaf647fbf76a88eb34146c47db53fa119f7a
mahankkon-blip <mahankko.n@gmail.com>
2026-07-29 12:32:02 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,79 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes,
+)
+
+TOKEN = "8805155186:AAEcBGMDf8Tb2FEptlNLbdB6U5Bkk81tCy8"
+
+CHANNEL = "@K_mahan_O"
+
+GAMES = [
+    ("🎮 نوستالژی", "https://play.google.com/store/apps/details?id=fi.twomenandadog.zombiecatchers"),
+    ("🎮 شبیه ساز زندگی", "https://play.google.com/store/apps/details?id=adventure.party.real.life"),
+    ("🎮 قایم موشک", "https://share.google/OQ7FHGUGtYxx1xGQ3"),
+    ("🎮 مهماندار هواپیما", "https://store.steampowered.com/app/4534960/Dear_Passengers/"),
+    ("🎮 چنل یوتیوب", "https://www.youtube.com/@mahanko44"),
+]
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "📢 عضویت در کانال",
+                url="https://t.me/K_mahan_O",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "✅ عضو شدم",
+                callback_data="check"
+            )
+        ],
+    ]
+
+    await update.message.reply_text(
+        "👋 سلام\n\n"
+        "برای دریافت لینک بازی ابتدا عضو کانال شوید و سپس روی «عضو شدم» بزنید.",
+        reply_markup=InlineKeyboardMarkup(keyboard),
+    )
+  async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    user_id = query.from_user.id
+
+    try:
+        member = await context.bot.get_chat_member(CHANNEL, user_id)
+
+        if member.status in ["member", "administrator", "creator"]:
+
+            keyboard = []
+
+            for name, link in GAMES:
+                keyboard.append([InlineKeyboardButton(name, url=link)])
+
+            await query.message.reply_text(
+                "✅ عضویت شما تایید شد.\n\n🎮 یکی از بازی‌های زیر را انتخاب کنید:",
+                reply_markup=InlineKeyboardMarkup(keyboard)
+            )
+
+        else:
+            await query.message.reply_text(
+                "❌ هنوز عضو کانال نیستید."
+            )
+
+    except Exception as e:
+        await query.message.reply_text(
+            "❌ خطا در بررسی عضویت. مطمئن شوید ربات ادمین کانال است."
+        )
+      app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(check, pattern="check"))
+
+print("🤖 Bot is running...")
+
+app.run_polling()
